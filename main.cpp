daa967c25bea0dfaf3dd53e3e11a3120d1abcf8c
sebastian5555 <44908078+sebastian5555@users.noreply.github.com>
2019-10-05 12:14:46 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,24 +1,107 @@
-#include "charstack.hpp"
-#include <iostream>
-using namespace std;
+#include "simple_vector.hpp"
 
-int main(){
-  CharStack myStack;
 
-  for(int i = 65; i < 78; ++i)
-    myStack.push(char(i));
-  cout << myStack.top() << endl;
+int main() {
+  SimpleVec myvec;
 
-  int mx = myStack.size();
+  //Initialize the vector
+  for(int i = 0; i < 20; i++)
+    myvec.push(i);
 
-  for(int i = 0; i < mx; ++i)
-    cout << myStack.pop() << " ";
+  //iterate with index
+  cout << "Going through the vector with the index:\n";
+  for(int i = 0; i < 20; i++)
+    cout << myvec.retrieve(i) << " ";
   cout << endl;
 
-  if(myStack.empty())
-    cout << "Stack is empty\n";
+  //Declare an iterator
+  SimpleVec::iterator it = myvec.begin();
 
-  myStack.empty();
-  myStack.top();
+  //iterate with iterator
+  cout << "Going through the vector with the iterator:\n";
+  for(it = myvec.begin(); it != myvec.end(); it++)
+    cout << *it << " ";#include "simple_vector.hpp"
+
+
+int main() {
+  SimpleVec myvec;
+
+  //Initialize the vector
+  for(int i = 0; i < 20; i++)
+    myvec.push(i);
+
+  //iterate with index
+  cout << "Going through the vector with the index:\n";
+  for(int i = 0; i < 20; i++)
+    cout << myvec.retrieve(i) << " ";
+  cout << endl;
+
+  //Declare an iterator
+  SimpleVec::iterator it = myvec.begin();
+
+  //iterate with iterator
+  cout << "Going through the vector with the iterator:\n";
+  for(it = myvec.begin(); it != myvec.end(); it++)
+    cout << *it << " ";
+  cout << endl;
+
+  //Go to the 3rd element
+  it = myvec.begin();
+  for(int i = 0; i < 3; i++)
+    ++it;
+  //Change the element
+  *it = 300;
+  //Delete the last 3 elements
+  for(int i = 0; i < 3; i++)
+    myvec.erase(myvec.size()-1);
+
+
+  //iterate with iterator
+  cout << "Going through the vector with the iterator:\n";
+  for(it = myvec.begin(); it != myvec.end(); it++)
+    cout << *it << " ";
+  cout << endl;
+
+  //Testing prefix/postfix operators
+  SimpleVec::iterator it2 = myvec.begin();
+  SimpleVec::iterator it3 = myvec.begin();
+
+  cout << "*it++: " << *it2++
+       << " *it: " << *it2 << endl;
+  cout << "++*it: " << *++it3
+       << " *it: " << *it3 << endl;
+
+
+  return 0;
+}
+  cout << endl;
+
+  //Go to the 3rd element
+  it = myvec.begin();
+  for(int i = 0; i < 3; i++)
+    ++it;
+  //Change the element
+  *it = 300;
+  //Delete the last 3 elements
+  for(int i = 0; i < 3; i++)
+    myvec.erase(myvec.size()-1);
+
+
+  //iterate with iterator
+  cout << "Going through the vector with the iterator:\n";
+  for(it = myvec.begin(); it != myvec.end(); it++)
+    cout << *it << " ";
+  cout << endl;
+
+  //Testing prefix/postfix operators
+  SimpleVec::iterator it2 = myvec.begin();
+  SimpleVec::iterator it3 = myvec.begin();
+
+  cout << "*it++: " << *it2++
+       << " *it: " << *it2 << endl;
+  cout << "++*it: " << *++it3
+       << " *it: " << *it3 << endl;
+
+
   return 0;
 }
